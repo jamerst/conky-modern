238ee67f6082f66c13681a73d2c1e2233fbc09a8
James Tattersall <10601770+jamerst@users.noreply.github.com>
2018-08-28 20:47:38 +0100

    Fix very weird bug where symlink to cover would be inserted in home directory when media paused

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cover=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$1 /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | egrep -A 1 "artUrl" | egrep -v "artUrl" | cut -b 44- | cut -d '"' -f 1 | egrep -v ^$ | cut -c8-`
-ln -s -f $cover ~/.conky/conky-modern/cover.png
+if [ `$HOME/.conky/conky-modern/scripts/state.sh yarock` != "Stopped" ]; then
+	cover=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.$1 /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | egrep -A 1 "artUrl" | egrep -v "artUrl" | cut -b 44- | cut -d '"' -f 1 | egrep -v ^$ | cut -c8-`
+	ln -s -f $cover $HOME/.conky/conky-modern/cover.png
+fi
